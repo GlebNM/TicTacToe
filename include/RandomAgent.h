716f71ee1a4f252a7722ff6c55e912cb60000206
B-a-r-c-i-k <kir100803@gmail.com>
2022-04-03 13:17:14 +0300

    Add random bot

---
+++ include/RandomAgent.h
@@ -0,0 +1,24 @@
+#include "Bot.h"
+
+class RandomAgent : public Bot {
+private:
+    State state;
+    
+public:
+
+    RandomAgent();
+
+    ~RandomAgent();
+
+    void init(const State&) final;
+
+    Position choseBestMove(int) final;
+
+    void performMove(Position) final;
+
+    void setState(const State&) final;
+
+    void setAllPossibleMoves(const std::vector<Position>&) final;
+
+    void resetGame() final;
+};
---
+++ src/RandomAgent.cpp
@@ -0,0 +1,34 @@
+#include "RandomAgent.h"
+#include <iostream>
+#include <random>
+
+
+
+void RandomAgent::init(const State& nState) {
+    state = nState;
+}
+
+void RandomAgent::resetGame() {
+    state.reset();
+}
+
+Position RandomAgent::choseBestMove(int timeLimit) {
+    std::vector<Position> allMoves = state.getAvailableMoves();
+    std::random_device rd;
+    std::mt19937 g(rd());
+    return allMoves[static_cast<int>(g()) % static_cast<int>(allMoves.size())];
+}
+
+void RandomAgent::performMove(Position move) {
+    state.performMove(move);
+}
+
+void RandomAgent::setState(const State& newState) {
+    state = newState;
+}
+
+void RandomAgent::setAllPossibleMoves(const std::vector<Position>&) {
+
+}
+
+
